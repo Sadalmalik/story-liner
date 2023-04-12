3611aa13b448eb9cccd4274c1d64d3bf5a122890
Gleb Kuzmin <gleb.kuzmin@geeky.house>
2023-04-13 01:28:11 +0200

    Add chapter node controller;

---
+++ Assets/Game/Modules/Core.Story/Runtime/Code/Player/Controllers/NodeChapterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Self.Architecture.IOC;
+
+namespace Self.Story
+{
+	public class NodeChapterController : SharedObject, INodeController
+	{
+		public Type   GetTargetType() => typeof(ChapterNode);
+
+		[Inject] private StoryController _storyController;
+		
+		public string Enter(BaseNode node)
+		{
+			var chapterNode = node as ChapterNode;
+			
+			_storyController.SetChapter(chapterNode.chapter, null);
+
+			return null;
+		}
+	}
+}
