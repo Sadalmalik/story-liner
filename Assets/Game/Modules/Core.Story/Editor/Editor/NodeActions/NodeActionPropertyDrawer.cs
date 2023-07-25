322ce87ae1d36168ac2b12f37527e1210bb07ce4
Dioinecail <andrewdionecail@gmail.com>
2023-07-25 22:38:51 +0200

    Minor tweak to NodeAction display, now it displays name

--- Assets/Game/Modules/Core.Story/Editor/Editor/NodeActions/NodeActionPropertyDrawer.cs
+++ Assets/Game/Modules/Core.Story/Editor/Editor/NodeActions/NodeActionPropertyDrawer.cs
@@ -24,13 +24,25 @@ namespace Self.Story.Editors
 	[CustomPropertyDrawer(typeof(BaseAction), true)]
 	public class NodeActionPropertyDrawer : PropertyDrawer
 	{
-		private const BindingFlags BINDING_FLAGS = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+		private const BindingFlags BINDING_FLAGS	= BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+		private StyleLength paddingBottom			= new StyleLength(new Length(3f, LengthUnit.Pixel));
+		private StyleLength paddingTop				= new StyleLength(new Length(3f, LengthUnit.Pixel));
+		private StyleLength paddingLeft				= new StyleLength(new Length(4f, LengthUnit.Pixel));
+		private StyleLength paddingRight			= new StyleLength(new Length(3f, LengthUnit.Pixel));
 
 
 		public override VisualElement CreatePropertyGUI(SerializedProperty property)
 		{
 			var serializedObject = new SerializedObject(property.objectReferenceValue);
 			var container        = new VisualElement() {name = property.displayName};
+
+			var label = new Label(property.objectReferenceValue.GetType().Name);
+			label.style.paddingLeft = paddingLeft;
+			label.style.paddingTop = paddingTop;
+			label.style.paddingBottom = paddingBottom;
+			label.style.paddingRight = paddingRight;
+
+			container.Add(label);
 
 			var fields = property.objectReferenceValue
 				.GetType()
